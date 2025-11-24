3eb1d9ed5d1f02e5f0d3185e0c40a3c57aa850bf
MXJXN <modulations@gmail.com>
2025-11-24 14:18:26 -0500

    fix: resolve TypeScript error in MarkdownRenderer component

--- apps/auctionhouse-docs/components/MarkdownRenderer.tsx
+++ apps/auctionhouse-docs/components/MarkdownRenderer.tsx
@@ -1,31 +1,31 @@
 'use client';
 
 import ReactMarkdown from 'react-markdown';
+import type { Components } from 'react-markdown';
 
 interface MarkdownRendererProps {
   content: string;
 }
 
 export default function MarkdownRenderer({ content }: MarkdownRendererProps) {
+  const components: Components = {
+    img: ({ ...props }) => (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img {...props} alt={props.alt || ''} />
+    ),
+    a: ({ ...props }) => {
+      const href = props.href || '';
+      // For relative links, we need to handle them specially
+      if (href.startsWith('http://') || href.startsWith('https://')) {
+        return <a {...props} target="_blank" rel="noopener noreferrer" />;
+      }
+      return <a {...props} />;
+    },
+  };
+
   return (
     <div className="markdown">
-      <ReactMarkdown
-        components={{
-          // Custom renderers for specific elements if needed
-          img: ({ node, ...props }) => (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img {...props} alt={props.alt || ''} />
-          ),
-          a: ({ node, ...props }) => {
-            const href = props.href || '';
-            // For relative links, we need to handle them specially
-            if (href.startsWith('http://') || href.startsWith('https://')) {
-              return <a {...props} target="_blank" rel="noopener noreferrer" />;
-            }
-            return <a {...props} />;
-          },
-        }}
-      >
+      <ReactMarkdown components={components}>
         {content}
       </ReactMarkdown>
     </div>
