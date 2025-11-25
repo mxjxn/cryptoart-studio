f1f73093f63d89ed2360d3f55c5e7a4b4d98e5b1
MXJXN <modulations@gmail.com>
2025-11-25 00:17:37 -0500

    Fix secret permissions: use su-exec to read secrets as root then switch to indexer user

--- packages/creator-core-indexer/docker-entrypoint.sh
+++ packages/creator-core-indexer/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Read secrets and construct environment variables
+# Read secrets and construct environment variables (as root, before switching users)
 if [ -f /run/secrets/postgres_password ]; then
   export POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
   export POSTGRES_URL="postgres://${POSTGRES_USER:-postgres}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB:-cryptoart}"
@@ -12,6 +12,7 @@ if [ -f /run/secrets/redis_password ]; then
   export REDIS_URL="redis://:${REDIS_PASSWORD}@redis:6379"
 fi
 
-# Execute the main command
-exec "$@"
+# Switch to indexer user and execute the main command
+# su-exec preserves environment variables and is lightweight
+exec su-exec indexer "$@"
 
